73177cfe77dc5d0e0983e2e9f60bbf4816536bfc
0390 <a0390@0390demac-mini.local>
2017-10-24 09:59:19 +0800

    Initial Commit

---
+++ Coding_Norma/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coding_Norma
+//
+//  Created by 0390 on 2017/10/24.
+//  Copyright © 2017年 norma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Coding_Norma/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Coding_Norma
+//
+//  Created by 0390 on 2017/10/24.
+//  Copyright © 2017年 norma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
